6b5f581ed4ecc3990f17091df82f913d15fd6e43
Vera <vera@DESKTOP-QPRLS4I.localdomain>
2022-06-06 11:44:37 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+const names = [ "Guadalupe, Ollie,Aki"];
+function writeCards(names){
+    for(let n=0;n<=names.length;n++) {console.log(`Thank you, ${names[n]}, for the wonderful surprise gift!`);
+debugger;
+}
+return writeCards;
+}
+function countDown(){
+    let n=11;
+    while(n>=1){
+        console.log(n-1);
+        n--;
+    }
+}
+function calledWithExactlylet(){
+   let  n=5;
+while(n>=1){
+    console.log(n-1);
+    n--;
+}
+}
+
